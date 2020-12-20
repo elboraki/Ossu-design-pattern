de687d0d6dd33c63b35a8dc27130d93cae18eab0
elboraki <younes.elboraki@gmail.com>
2020-12-20 23:14:10 +0100

    calculate app

--- Principles/polymorphisme/index.php
+++ Principles/polymorphisme/index.php
@@ -5,6 +5,6 @@ require_once '../../vendor/autoload.php';
 
 $square = new Square(300);
 $circle = new Circle(200);
-echo ('Square area' . $square->calculateArea());
+echo ('Square area  ' . $square->calculateArea());
 echo "\n";
-echo ('Circle area' . $circle->calculateArea());
+echo ('Circle area ' . $circle->calculateArea());
