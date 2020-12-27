a119e4c87a2726c78944f8ca0d5731b2c96291e8
elboraki <younes.elboraki@gmail.com>
2020-12-27 23:57:20 +0100

    enhance using methods

--- Principles/factoryPattern/index.php
+++ Principles/factoryPattern/index.php
@@ -5,6 +5,8 @@ require_once '../../vendor/autoload.php';
 $pizeria = new Pizzeria();
 $pf = $pizeria->orderPizza('fromage');
 $pfm = $pizeria->orderPizza('fruit mer');
+
+var_dump($pfm->prepare());
 var_dump($pf->pack());
 var_dump($pfm->prepare());
-var_dump($pfm->bake());
+var_dump($pfm->pack());
